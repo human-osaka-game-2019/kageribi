21c2480e764a70424ade6d8ecb72b1b14046b714
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-10-26 08:40:32 +0900

    画像読み込みに必要な関数の宣言 #7

---
+++ kageribi/Texture.h
@@ -0,0 +1,20 @@
+﻿#ifndef TEXTURE_H_
+#define TEXTURE_H_
+
+#include <Windows.h>
+#include <d3d9.h>
+
+typedef struct
+{
+	LPDIRECT3DTEXTURE9 TextureDate;
+	int width;
+	int height;
+} Texture;
+
+bool IsCategoryRangeCheck(int category_id, int texture_id);
+
+bool LoadTexture(const char* file_name, int category_id, int texture_id);
+
+bool CreateTexture(const char* file_name, Texture* texture_data);
+
+#endif
