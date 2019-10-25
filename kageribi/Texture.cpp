7b1461b82b66efcce08e0789d4016b4b806d344c
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-10-26 08:40:32 +0900

    画像読み込みに必要な関数の宣言 #7

---
+++ kageribi/Texture.cpp
@@ -0,0 +1,19 @@
+﻿#include "Texture.h"
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+bool IsCategoryRangeCheck(int category_id, int texture_id)
+{
+	return false;
+}
+
+bool LoadTexture(const char* file_name, int category_id, int texture_id)
+{
+	return false;
+}
+
+bool CreateTexture(const char* file_name, Texture* texture_data)
+{
+	return 0;
+}
